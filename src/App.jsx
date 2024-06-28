146f7b0e970f515404f30cdd071fa4698b26260c
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-06-28 10:13:16 +0530

    React Projects & Interview Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>React Projects & Interview Questions</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
